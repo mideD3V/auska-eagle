e99396ca4997276f2ad4d89d633645d984e3b7de
mided3v <ayo4real6@gmail.com>
2022-10-14 11:57:30 +0100

    initialized the project

---
+++ index.js
@@ -0,0 +1,18 @@
+// on click
+// if more-text === none
+// change the display to inline
+
+
+function readMore() {
+    var moreText = document.querySelector('#more-text')
+    var readMore = document.querySelector('#read-more');
+
+    if (moreText.style.display === 'none') {
+        moreText.style.display = 'inline';
+        readMore.innerHTML = '...Read less'
+    } else {
+        moreText.style.display = "none";
+    }
+}
+
+
